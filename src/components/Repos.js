f169e24b6f95715df821429be91f334ba0263d5f
Young Han Lee <joybro201@gmail.com>
2017-02-12 20:28:12 +0000

    Add repo navigation button
    https://github.com/reactjs/react-router-tutorial/tree/master/lessons/12-navigating

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import NavLink from './NavLink'
+import { browserHistory } from 'react-router'
 
 class Repos extends React.Component {
+  handleSubmit(event) {
+    event.preventDefault();
+    const userName = event.target.elements[0].value;
+    const repo = event.target.elements[1].value;
+    const path = `/repos/${userName}/${repo}`;
+
+    browserHistory.push(path);
+  };
+
   render() {
     return (
       <div>
@@ -10,6 +20,14 @@ class Repos extends React.Component {
         <ul>
           <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
           <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+
+          <li>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="userName"/> / {' '}
+              <input type="text" placeholder="repo"/>{' '}
+              <button type="submit">Go</button>
+            </form>
+          </li>
         </ul>
 
         {this.props.children}
