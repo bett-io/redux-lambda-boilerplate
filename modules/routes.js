e3282b5d6faff845461bb34602c6301952bab777
Bongseok Choi <bongseok13.choi@lge.com>
2017-03-03 09:49:13 +0900

    Capitalize component name
    In routes.js for only linux.

--- modules/routes.js
+++ modules/routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import App from '../src/components/app';
+import App from '../src/components/App';
 import Home from '../src/components/Home';
 import Repos from '../src/components/Repos';
 import Repo from '../src/components/Repo';
-import About from '../src/components/about';
-import Hello from '../src/components/hello';
+import About from '../src/components/About';
+import Hello from '../src/components/Hello';
 
 module.exports = (
   <Route path="/" component={App}>
