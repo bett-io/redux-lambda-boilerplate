219c34cbc35ad9f1b6b395059f7535a43d52c8b0
Bongseok Choi <bongseok13.choi@lge.com>
2017-04-13 13:59:48 +0900

    Fix lint warnings and errors
    Add mocha env.
    Ignore standalone/ directory.

--- test/App-test.js
+++ test/App-test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { expect } from 'chai';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import App from '../src/components/App';
 
 describe('<App />', () => {
