366ec479047510abb13eb666403123e456bfbf57
ChangBok Chea <changbok.chea@gmail.com>
2017-05-07 15:32:22 +0900

    Fix esLint comma-dangle error
    pleaes refer to http://eslint.org/docs/rules/comma-dangle

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -43,7 +43,7 @@ module.exports = {
   resolve: {
     alias: {
       'social.config.json': __dirname + '/config/social.config.' + nodeEnvPath + '.json',
-      'aws.config.json': __dirname + '/config/aws.config.' + nodeEnvPath + '.json'
-    }
+      'aws.config.json': __dirname + '/config/aws.config.' + nodeEnvPath + '.json',
+    },
   },
 };
