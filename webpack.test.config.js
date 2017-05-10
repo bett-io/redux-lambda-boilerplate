bfb23c9cfea38f6d9d1396d2190c2f4d6c49d543
Bongseok Choi <bongseok13.choi@lge.com>
2017-05-10 13:14:21 +0900

    Use mocha-webpack instead of mocha for test
    mocha-webpack for testing to use webpack configuration file.

---
+++ webpack.test.config.js
@@ -0,0 +1,11 @@
+var clientConfig = require('./webpack.client.config.js');
+
+clientConfig.externals = {
+  'react/addons': true,  // pre-existing at enzyme 2.8.0
+  'react/lib/ExecutionEnvironment': true,  // pre-existing at enzyme 2.8.0
+  'react/lib/ReactContext': true,  // pre-existing at enzyme 2.8.0
+  'react-dom/test-utils': true,
+  'react-test-renderer/shallow': true,
+};
+
+module.exports = clientConfig;
