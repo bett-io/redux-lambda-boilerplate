9a35b5bace156e45f44091e10d9072c042a304f3
Young Han Lee <joybro201@gmail.com>
2017-08-27 11:21:37 +0100

    Fix test failure
    The test failed as Header components had been moved to its own file
    from App.

---
+++ test/Header-test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import Header from '../src/components/Header';
+
+describe('<Header />', () => {
+  it('contains 3 <NavItem />s', function() {
+    expect(shallow(<Header />).find('NavItem')).to.have.lengthOf(3);
+  });
+  it('contains links to about, repos and hello', function() {
+    var component = shallow(<Header />).find('LinkContainer');
+    expect(component.findWhere(n => n.props().to === '/about')).to.have.lengthOf(1);
+    expect(component.findWhere(n => n.props().to === '/repos')).to.have.lengthOf(1);
+    expect(component.findWhere(n => n.props().to === '/repo')).to.have.lengthOf(0);
+    expect(component.findWhere(n => n.props().to === '/hello')).to.have.lengthOf(1);
+  });
+});
