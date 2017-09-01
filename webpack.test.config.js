f14d6f58ee6bfb769cabc3d46c45cec8a0209a70
Bongseok Choi <bongseok13.choi@lge.com>
2017-09-01 09:03:24 +0900

    Fixes build fail

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -1,11 +1,15 @@
-var clientConfig = require('./webpack.client.config.js');
+const merge = require('webpack-merge');
+const clientConfig = require('./webpack.client.config.js');
 
-clientConfig.externals = {
-  'react/addons': true,  // pre-existing at enzyme 2.8.0
-  'react/lib/ExecutionEnvironment': true,  // pre-existing at enzyme 2.8.0
-  'react/lib/ReactContext': true,  // pre-existing at enzyme 2.8.0
-  'react-dom/test-utils': true,
-  'react-test-renderer/shallow': true,
+const testConfig = {
+  externals: {
+    'react/addons': true,  // pre-existing at enzyme 2.8.0
+    'react/lib/ExecutionEnvironment': true,  // pre-existing at enzyme 2.8.0
+    'react/lib/ReactContext': true,  // pre-existing at enzyme 2.8.0
+    'react-dom/test-utils': true,
+    'react-test-renderer/shallow': true,
+  },
 };
 
-module.exports = clientConfig;
+module.exports = merge(testConfig, clientConfig());
+
